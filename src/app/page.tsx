8c41bac5715cf0af904bcb4b39c69d0b9e21cb97
Soyeb Islam <soyeb.islam117@gmail.com>
2025-09-04 15:34:33 +0600

    feat(auth): implement NextAuth with Prisma, Google & GitHub providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AuthButtons from "@/components/AuthButtons";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
+      <AuthButtons />
     </div>
   );
 }
