4555f6672f708d4fe1459ff7485609289f1b387b
Soyeb Islam <soyeb.islam117@gmail.com>
2025-09-06 16:38:32 +0600

    add navbar to all pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import CoreFeatures from "@/components/Features/Features";
 import Hero from "@/components/Hero/Hero";
 import HowItWorks from "@/components/HowItWorks/HowItWorks";
-import Navbar from "@/components/Navbar/Navbar";
 import ToolSection from "@/components/ToolSection/ToolSection";
 
 export default function Home() {
   return (
     <main className="">
-      <Navbar />
       <Hero />
       <ToolSection />
       <CoreFeatures />
