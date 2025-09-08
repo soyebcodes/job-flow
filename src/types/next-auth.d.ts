3b60d19e2fad248012eff3e941f3c8624233f700
Soyeb Islam <soyeb.islam117@gmail.com>
2025-09-09 03:18:31 +0600

    add type defination

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,13 +2,12 @@ import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
-    user?: {
-      id?: string;
+    user: {
+      id: string; // now required
       email?: string | null;
       name?: string | null;
       image?: string | null;
     } & DefaultSession["user"];
-
     // Supabase-compatible JWT
     supabaseAccessToken?: string;
   }
@@ -16,6 +15,6 @@ declare module "next-auth" {
 
 declare module "next-auth/jwt" {
   interface JWT {
-    supabaseAccessToken?: string;
+    id?: string; // store user id in JWT
   }
 }
